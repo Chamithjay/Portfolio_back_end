687c9b47be75b0339373833d99803e1716b0bfae
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2025-01-06 23:06:22 +0530

    Prepare for Vercel deployment

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import 'dotenv/config';
-import projectRoutes from './routes/projects.js';
-import skilltRoutes from './routes/skills.js';
-import usertRoutes from './routes/user.js';
-import contactRoutes from './routes/contact.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import projectRoutes from "./routes/projects.js";
+import skilltRoutes from "./routes/skills.js";
+import usertRoutes from "./routes/user.js";
+import contactRoutes from "./routes/contact.js";
 
 const app = express();
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN,  // Use the environment variable for the frontend URL
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  };
-  
 
+app.get("/", (req, res) => {
+  res.send("Hello, Vercel!");
+});
+
+module.exports = app;
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN, // Use the environment variable for the frontend URL
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+};
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
-
-app.use('/projects', projectRoutes);
-app.use('/skills', skilltRoutes);
-app.use('/user', usertRoutes);
-app.use('/contact', contactRoutes);
-
+app.use("/projects", projectRoutes);
+app.use("/skills", skilltRoutes);
+app.use("/user", usertRoutes);
+app.use("/contact", contactRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vv7aw.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0`;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
-
-
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
