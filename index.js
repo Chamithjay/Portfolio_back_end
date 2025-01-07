e6273c27f130f660df26a38565a736e49071560c
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2025-01-07 23:54:33 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
 app.get("/", (req, res) => {
